1f0f58730a590fd49a18de3d403f3b7ba078dbef
Viren Shinde <viren.shinde@cigna.com>
2016-08-16 15:37:44 -0400

    Xcode project creation

---
+++ GitTest/GitTest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Viren Shinde on 8/16/16.
+//  Copyright © 2016 Viren Shinde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
